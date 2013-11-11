2d5e5060479e441de0701e5d4c77832a215e3a5f
miks <miks@cubesystems.lv>
2013-11-11 11:21:01 +0200

    Validation: add "data-validation-initialized" for initalized forms

--- app/assets/javascripts/releaf/include/common.js
+++ app/assets/javascripts/releaf/include/common.js
@@ -440,7 +440,8 @@ jQuery(function(){
         });
 
 
-
+        // validation initalized finished, add data attribute for it (used by automatized test, etc)
+        form.attr("data-validation-initialized", true);
 
     });
 
