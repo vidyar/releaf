25b79fb2dc41e9e878d8e2b2869c2ea2bd3186b4
Aldis Berjoza <graudeejs@yandex.ru>
2013-12-05 09:52:51 +0200

    BaseController#success_url, #build_breadcrumbs, #add_resource_breadcrumb should be protected

--- app/controllers/releaf/base_controller.rb
+++ app/controllers/releaf/base_controller.rb
@@ -563,6 +563,42 @@ module Releaf
       return resource_class.table_name + '.' + params[:order_by].sub(/-reverse$/, ' DESC')
     end
 
+    # Returns url to redirect after successul resource create/update actions
+    #
+    # @return [String] url
+    def success_url
+      url_for( action: 'edit', id: @resource.id )
+    end
+
+    def build_breadcrumbs
+      @breadcrumbs = []
+      @breadcrumbs << { name: I18n.t('Home', scope: 'admin.breadcrumbs'), url: releaf_root_path }
+
+      controller_params = Releaf.controller_list[self.class.name.sub(/Controller$/, '').underscore]
+      unless controller_params.nil?
+        @breadcrumbs << {
+          name: I18n.t(controller_params[:name], scope: "admin.menu_items"),
+          url: send(controller_params[:url_helper])
+        }
+      end
+    end
+
+
+    def add_resource_breadcrumb resource, url = nil
+      if resource.new_record?
+        name=  I18n.t('New record', scope: 'admin.breadcrumbs')
+        url = url_for(action: :new, only_path: true) if url.nil?
+      else
+        if resource.respond_to?(:to_text)
+          name = resource.send(:to_text)
+        else
+          name = I18n.t('Edit record', scope: 'admin.breadcrumbs')
+        end
+        url = url_for(action: :edit, id: resource.id, only_path: true) if url.nil?
+      end
+      @breadcrumbs << { name: name, url: url }
+    end
+
     private
 
     def check_feature feature
@@ -679,41 +715,6 @@ module Releaf
       filter_templates_from_hash(params)
     end
 
-    # Returns url to redirect after successul resource create/update actions
-    #
-    # @return [String] url
-    def success_url
-      url_for( action: 'edit', id: @resource.id )
-    end
-
-    def build_breadcrumbs
-      @breadcrumbs = []
-      @breadcrumbs << { name: I18n.t('Home', scope: 'admin.breadcrumbs'), url: releaf_root_path }
-
-      controller_params = Releaf.controller_list[self.class.name.sub(/Controller$/, '').underscore]
-      unless controller_params.nil?
-        @breadcrumbs << {
-          name: I18n.t(controller_params[:name], scope: "admin.menu_items"),
-          url: send(controller_params[:url_helper])
-        }
-      end
-    end
-
-
-    def add_resource_breadcrumb resource, url = nil
-      if resource.new_record?
-        name=  I18n.t('New record', scope: 'admin.breadcrumbs')
-        url = url_for(action: :new, only_path: true) if url.nil?
-      else
-        if resource.respond_to?(:to_text)
-          name = resource.send(:to_text)
-        else
-          name = I18n.t('Edit record', scope: 'admin.breadcrumbs')
-        end
-        url = url_for(action: :edit, id: resource.id, only_path: true) if url.nil?
-      end
-      @breadcrumbs << { name: name, url: url }
-    end
 
     def filter_templates_from_array arr
       return unless arr.is_a? Array
